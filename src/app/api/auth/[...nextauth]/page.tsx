96bb4e04d6c4608e806a5fd55a489947e8551af7
TapasxKofuku <tapas@kofukuhcs.com>
2025-08-06 11:16:10 +0530

    adding on callback page

---
+++ src/app/api/auth/[...nextauth]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+    console.log("apple sign in callback page");
+  return (
+    <div>apple sign in callback page</div>
+  )
+}
+
+export default page
